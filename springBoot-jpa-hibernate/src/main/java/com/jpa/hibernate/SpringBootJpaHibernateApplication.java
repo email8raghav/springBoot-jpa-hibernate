49015d2f4ea47d1b60968b70e964f636a5443b70
raghavendra <email8raghav@gmail.com>
2021-06-17 00:55:17 +0530

    push p[ush

---
+++ springBoot-jpa-hibernate/src/main/java/com/jpa/hibernate/SpringBootJpaHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHibernateApplication.class, args);
+		
+	}
+
+}
